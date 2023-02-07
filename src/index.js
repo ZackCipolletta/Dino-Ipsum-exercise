993b6900a5c724c3757c252bdfa7f0d6251291a8
Zachary Cipolletta <ZackCipolletta@gmail.com>
2023-02-07 13:07:10 -0800

    more change?

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './css/styles.css';
 // import DinoIpsumGenerator from './js/dinoIpsumGenerator.js';
 
 function getDino() {
-  let promise = new Promise(function(resolve, reject) {
+  let promise = new Promise(function (resolve, reject) {
     let request = new XMLHttpRequest();
     const url = `https://dinoipsum.com/api/?format=json&paragraphs=1&words=1`;
     request.addEventListener("loadend", function () {
@@ -18,27 +18,31 @@ function getDino() {
     request.open("GET", url, true);
     request.send();
   });
-  promise.then(function(response) {
+  promise.then(function (response) {
     printDinos(response);
-  }, function(errorMEssage) {
+    callCompareUserInput(response);
+  }, function (errorMEssage) {
     printError(errorMEssage);
   });
 }
 
 function printDinos(response) {
-  console.log(response[0].toString().split(''));
+  console.log(response[0][0].split(''));
   let ul = document.getElementById('response');
   let i = 0;
 
   let responseArr = response[0].toString().split('');
-  responseArr.forEach(element => {
+  for (let x = 0; x < responseArr.length; x++) {
     let pEl = document.createElement('p');
-    pEl.innerText = element;
-    pEl.setAttribute("id", `${i}`)
-    pEl.setAttribute("class", "hidden")
+    let pEl2 = document.createElement('p');
+    pEl.innerText = `_`;
+    pEl2.innerText = ' ';
+    pEl.setAttribute("id", `${i}`);
+    // pEl.setAttribute("class", "hidden");
     ul.append(pEl);
-    i++
-  });
+    ul.append(pEl2);
+    i++;
+  }
   return (response[0].toString().split(''));
 }
 
@@ -46,23 +50,32 @@ function printError(error) {
   console.log(error);
 }
 
+function callCompareUserInput(x) {
+  document.getElementById('userInputForm').addEventListener("submit", function (e) {
+    e.preventDefault();
+    compareUserInput(x);
+  });
+}
+
 function compareUserInput(response) {
-  let userInput = document.getElementById("text");
+  let userInput = document.getElementById("text").value;
   let i = 0;
-  console.log(response)
-  response.forEach(element => {
-    i++
-    if(element.toUpperCase() === userInput.toUpperCase()) {
-      document.getElementById(`${i}`).removeAttribute("class")
+  let thisResponse = response.toString().split('');
+  console.log(userInput);
+  thisResponse.forEach(element => {
+    if (element.toUpperCase() === userInput.toUpperCase()) {
+      console.log(i)
+      console.log('hi')
+      document.getElementById(`${i}`).innerText = element;
     }
-  })
+    i++;
+  });
+}
 
-  }
-
-window.onload = function() {
+window.onload = function () {
   getDino();
-  let button = document.getElementById("submit");
-  button.onclick = function() {
-    compareUserInput();
-  }
+  // let button = document.getElementById("submit");
+  // button.onclick = function() {
+  //   compareUserInput();
+  // }
 }
